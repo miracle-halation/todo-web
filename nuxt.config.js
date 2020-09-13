52fb66cd3f01bff499b1a61190913b2c00b8b962
miracle-halation <miraclehalation.9710@gmail.com>
2020-09-13 10:50:44 +0900

    キーを環境変数に変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 import colors from 'vuetify/es5/util/colors'
+require('dotenv').config();
 
 export default {
   /*
@@ -49,6 +50,7 @@ export default {
   */
   buildModules: [
     '@nuxtjs/vuetify',
+    '@nuxtjs/dotenv',
   ],
   /*
   ** Nuxt.js modules

2298f5fa3f12a7f5b26d7e333790f4272da4e782
miracle-halation <miraclehalation.9710@gmail.com>
2020-09-13 15:02:24 +0900

    フロント側で新規登録ができる

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,15 +1,15 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 
-const fbConfig = {
-  API_KEY: process.env.API_KEY,
-  AUTH_DOMAIN: process.env.AUTH_DOMAIN,
-  DATABASE_URL: process.env.DATABASE_URL,
-  PROJECT_ID: process.env.PROJECT_ID,
-  STORAGE_BUCKET: process.env.STORAGE_BUCKET,
-  MESSAGE_SENDER_ID: process.env.MESSAGE_SENDER_ID,
-  APP_ID: process.env.APP_ID
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
 };
-firebase.initializeApp(fbConfig)
+firebase.initializeApp(firebaseConfig)
 
 export default firebase

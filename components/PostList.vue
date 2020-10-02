cf9bbe6e532f8d0d4482e8a52e7576c15b8401ca
miracle-halation <miraclehalation.9710@gmail.com>
2020-10-02 10:46:48 +0900

    postのページを作成

---
+++ components/PostList.vue
@@ -0,0 +1,42 @@
+<template>
+	<!-- <div>
+		<div v-for='post in posts'> -->
+			<v-card
+				class="mx-auto"
+				max-width="400"
+			>
+				<v-img
+					class="white--text align-end"
+					height="200px"
+					src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+				>
+					<v-card-title>Top 10 Australian beaches</v-card-title>
+				</v-img>
+
+				<v-card-text class="text--primary">
+
+					<div>Whitehaven Beach</div>
+
+				</v-card-text>
+
+			</v-card>
+		<!-- </div>
+	</div> -->
+</template>
+
+<script>
+	import axios from "@/plugins/axios";
+	export default {
+		props:["posts"],
+		data() {
+			return {
+
+			}
+		},
+		computed:{
+			user(){
+				return this.$store.state.currentUser;
+			}
+		},
+	}
+</script>
---
+++ pages/posts.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="user">
+    <p>{{user.name}}</p>
+		<PostList :posts="user.posts" />
+    <AddPost />
+  </div>
+</template>
+
+<script>
+import AddPost from '~/components/AddPost'
+import PostList from '~/components/PostList'
+import axios from '~/plugins/axios';
+
+export default {
+  components: {
+    AddPost,
+		PostList
+  },
+  data(){
+    return{
+      posts:[]
+    }
+  },
+  computed: {
+    user(){
+      return this.$store.state.currentUser;
+    }
+  },
+};
+</script>
